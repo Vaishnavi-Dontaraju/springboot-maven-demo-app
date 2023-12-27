6dce36fa5b5c1a157b9e07861cd449f454ce3757
Vaishnavi-Dontaraju <vaishu.dontaraju@gmail.com>
2023-12-26 23:13:15 -0400

    Creating initial springboot setup

---
+++ src/test/java/com/example/sb/mvn/demo/app/SpringbootMavenDemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sb.mvn.demo.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMavenDemoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
